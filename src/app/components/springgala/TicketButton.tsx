cd0f796b576d19fa69155c2ae46a7d74f557f4db
phil <philip@fortunehomesteam.com>
2025-04-10 18:25:23 +0800

    Update Ticket Button

--- src/app/components/springgala/TicketButton.tsx
+++ src/app/components/springgala/TicketButton.tsx
@@ -4,10 +4,31 @@ export default function TicketButton() {
   return (
     <a
       href=" https://join.feoaf.org/springgala2025"
-      className="mx-auto p-4 bg-neutral text-neutral-content hover:-translate-y-1 active:translate-y-0 cursor-pointer"
+      className="mx-auto flex items-center justify-center z-10 p-4 rounded-sm
+      bg-neutral text-neutral-content hover:scale-[105%] duration-100 active:scale-[95%] group cursor-pointer
+      relative overflow-hidden border-2 border-[#09090b]"
       target="_blank"
       rel="noopener noreferrer"
     >
+      <span>
+        <svg
+          className="w-6 h-6 mr-4 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M18.5 12A2.5 2.5 0 0 1 21 9.5V7a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v2.5a2.5 2.5 0 0 1 0 5V17a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1v-2.5a2.5 2.5 0 0 1-2.5-2.5Z"
+          />
+        </svg>
+      </span>
       Buy a ticket
     </a>
   );
